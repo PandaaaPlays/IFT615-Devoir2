36594d3b77d8adef609d0117aabc0c250433ca72
MathieuFortier2q <mathieupichu@gmail.com>
2024-07-29 11:11:33 -0400

    Can now successfully do all operations

--- Main.py
+++ Main.py
@@ -27,8 +27,11 @@ class ROCKET(OBJECT):
     def __init__(self, name):
         super().__init__(name)
         self.has_fuel = False
+        self.cargo = None
     def set_has_fuel(self, has_fuel):
         self.has_fuel = has_fuel
+    def set_cargo(self, cargo):
+        self.cargo = cargo
 
 class Operator:
     def __init__(self, name):
@@ -44,6 +47,26 @@ class Operator:
     def add_effect(self, effect):
         self.effects.append(effect)
 
+class Parameter:
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+
+class Precondition:
+    def __init__(self, operand, object_types):
+        self.operand = operand
+        self.object_types = object_types
+
+class Effect:
+    def __init__(self, operand, object_types):
+        self.operand = operand
+        self.object_types = object_types
+
+class VariablePair:
+    def __init__(self, variable, value):
+        self.variable = variable
+        self.value = value
+
 def read_lines(file_path):
     lines = []
     with open(file_path, 'r') as file:
@@ -54,10 +77,110 @@ def read_lines(file_path):
             lines.append(line)
     return lines
 
-def find_fact(list, name):
+def find_in_list(list, name):
     for fact in list:
         if fact.name == name:
             return fact
+    raise ValueError(f"{name} not found in the list")
+
+def get_class_by_name(name):
+    return globals()[name]
+
+def find_with_type(list, type_str):
+    type_class = get_class_by_name(type_str.upper())
+    for fact in list:
+        if isinstance(fact, type_class):
+            return fact
+    raise ValueError(f"{type_str} not found in the list")
+def find_with_variable(list, variable):
+    for var in list:
+        if var.variable.name == variable:
+            return var
+    raise ValueError(f"{variable} not found in the list")
+
+
+def check_params(op_params, params):
+    if len(op_params) != len(params):
+        raise ValueError("Number of parameters given do not match the number of operator parameters")
+    list = []
+
+    for op_param, param in zip(op_params, params):
+        if not isinstance(param, eval(op_param.type)):
+            raise ValueError(f"Parameter {param.name} does not match the expected type {op_param.type}")
+        else:
+            list.append(VariablePair(op_param, param))
+
+    return list
+
+def check_precond(op_precond, params):
+    match op_precond.operand:
+        case 'at':
+            type1 = op_precond.object_types[0]
+            obj1 = find_with_variable(params, type1)
+            type2 = op_precond.object_types[1]
+            obj2 = find_with_variable(params, type2)
+            if obj1.value.location.name != obj2.value.name:
+                raise ValueError(f"Object {obj1.name} is not at the location {obj2.name}")
+            else:
+                return True
+        case 'in':
+            type1 = op_precond.object_types[0]
+            obj1 = find_with_variable(params, type1)
+            type2 = op_precond.object_types[1]
+            obj2 = find_with_variable(params, type2)
+            if obj2.value.cargo.name != obj1.value.name:
+                raise ValueError(f"Object {obj1.name} is not in the rocket {obj2.name}")
+            else:
+                return True
+        case 'has-fuel':
+            type1 = op_precond.object_types[0]
+            obj1 = find_with_variable(params, type1)
+            if not obj1.value.has_fuel:
+                raise ValueError(f"Object {obj1.name} has no fuel")
+            else:
+                return True
+def apply_effect(op_effect, params, facts):
+    match op_effect.operand:
+        case '(in':
+            type1 = op_effect.object_types[0].strip('()')
+            obj1 = find_with_variable(params, type1)
+            type2 = op_effect.object_types[1].strip('()')
+            obj2 = find_with_variable(params, type2)
+            find_in_list(facts, obj2.value.name).set_cargo(obj1.value)
+        case '(del':
+            match op_effect.object_types[0]:
+                case 'at':
+                    type1 = op_effect.object_types[1].strip('()')
+                    obj1 = find_with_variable(params, type1)
+                    type2 = op_effect.object_types[2].strip('()')
+                    obj2 = find_with_variable(params, type2)
+                    if(obj1.value.location.name == obj2.value.name):
+                        find_in_list(facts, obj1.value.name).set_location(None)
+                case 'in':
+                    type1 = op_effect.object_types[2].strip('()')
+                    obj1 = find_with_variable(params, type1)
+                    find_in_list(facts, obj1.value.name).set_cargo(None)
+                case 'has-fuel':
+                    type1 = op_effect.object_types[1].strip('()')
+                    obj1 = find_with_variable(params, type1)
+                    find_in_list(facts, obj1.value.name).set_has_fuel(False)
+        case '(at':
+            type1 = op_effect.object_types[0].strip('()')
+            obj1 = find_with_variable(params, type1)
+            type2 = op_effect.object_types[1].strip('()')
+            obj2 = find_with_variable(params, type2)
+            find_in_list(facts, obj1.value.name).set_location(obj2.value)
+            print("we have set the location of " + obj1.value.name + " to " + obj2.value.name)
+
+
+def handle_operation(operator, params, facts):
+    params_list = check_params(operator.params, params)
+    for precond in operator.preconds:
+        check_precond(precond, params_list)
+    for effect in operator.effects:
+        apply_effect(effect, params_list, facts)
+    print("We made it here!")
+
 
 def graphplan(r_ops_file, r_facts_file):
     # Read all lines from each file until an empty line or EOF is encountered
@@ -85,11 +208,11 @@ def graphplan(r_ops_file, r_facts_file):
         parts = file_facts[counter].split()
         argument = parts[0].strip('()')
         if(argument == 'at'):
-            object = find_fact(facts, parts[1])
-            place = find_fact(facts, parts[2].strip('()'))
+            object = find_in_list(facts, parts[1])
+            place = find_in_list(facts, parts[2].strip('()'))
             object.set_location(place)
         if(argument == 'has-fuel'):
-            rocket = find_fact(facts, parts[1].strip('()'))
+            rocket = find_in_list(facts, parts[1].strip('()'))
             rocket.set_has_fuel(True)
         counter += 1
 
@@ -97,8 +220,8 @@ def graphplan(r_ops_file, r_facts_file):
 
     while counter < len(file_facts):
         parts = file_facts[counter].split()
-        cargo = find_fact(facts, parts[1].strip('()'))
-        place = find_fact(facts, parts[2].strip('()'))
+        cargo = find_in_list(facts, parts[1].strip('()'))
+        place = find_in_list(facts, parts[2].strip('()'))
         cargo.set_destination(place)
         counter += 1
 
@@ -110,45 +233,69 @@ def graphplan(r_ops_file, r_facts_file):
         counter += 2
         parts = re.findall(r'\([^)]*\)', file_operators[counter])
         for args in parts:
-            op.add_param(args)
+            arg_name = args.split()[0].strip('()')
+            arg_type = args.split()[1].strip('()')
+            param = Parameter(arg_name, arg_type)
+            op.add_param(param)
         counter += 2
         parts = re.findall(r'\([^)]*\)', file_operators[counter])
         for args in parts:
-            op.add_precond(args)
+            split = args.split()
+            operator = None
+            arguments = []
+            arg_counter = 0
+            for arg in split:
+                if arg_counter == 0:
+                    operator = arg.strip('()')
+                else:
+                    arguments.append(arg.strip('()'))
+                arg_counter += 1
+            op.add_precond(Precondition(operator, arguments))
         counter += 2
         parts = re.findall(r'\([^)]*\)', file_operators[counter])
         for args in parts:
-            op.add_effect(args)
+            split = args.split()
+            operator = None
+            arguments = []
+            arg_counter = 0
+            for arg in split:
+                if arg_counter == 0:
+                    operator = arg
+                else:
+                    arguments.append(arg)
+                arg_counter += 1
+            op.add_effect(Effect(operator, arguments))
         counter += 1
         operators.append(op)
 
 
-    print("Operators:")
-    for op in operators:
-        print("Name: " + op.name)
-        print("Params:")
-        for param in op.params:
-            print(param)
-        print("Preconds:")
-        for precond in op.preconds:
-            print(precond)
-        print("effects:")
-        for effect in op.effects:
-            print(effect)
-    print("Facts:")
-    for fact in facts:
-        print("Name: " + fact.name)
-        if isinstance(fact, OBJECT):
-            print("Location: " + fact.location.name)
-        if isinstance(fact, ROCKET):
-            print("Has fuel: " + str(fact.has_fuel))
-        if isinstance(fact, CARGO):
-            print("Destination: " + fact.destination.name)
 
+    #print("Facts:")
+    #for fact in facts:
+     #   print("Name: " + fact.name)
+      #  if isinstance(fact, OBJECT):
+       #     print("Location: " + fact.location.name)
+        #if isinstance(fact, ROCKET):
+         #   print("Has fuel: " + str(fact.has_fuel))
+        #if isinstance(fact, CARGO):
+         #   print("Destination: " + fact.destination.name)
 
-    # Implement your Graphplan logic here
-
-    # Example of an optimal plan (empty for now)
+    params_list = [find_in_list(facts, 'alex'), find_in_list(facts, 'r1'), find_in_list(facts, 'London')]
+    handle_operation(find_in_list(operators, 'LOAD'), params_list, facts)
+    print(params_list[1].cargo.name)
+    print(params_list[0].location)
+    handle_operation(find_in_list(operators, 'UNLOAD'), params_list, facts)
+    print(params_list[0].location.name)
+    print(params_list[1].cargo)
+    handle_operation(find_in_list(operators, 'LOAD'), params_list, facts)
+    params_list = [find_in_list(facts, 'r1'), find_in_list(facts, 'London'), find_in_list(facts, 'Paris')]
+    handle_operation(find_in_list(operators, 'MOVE'), params_list, facts)
+    print(params_list[0].location.name)
+    print(params_list[0].cargo.name)
+    print(params_list[0].cargo.location)
+    params_list = [find_in_list(facts, 'alex'), find_in_list(facts, 'r1'), find_in_list(facts, 'Paris')]
+    handle_operation(find_in_list(operators, 'UNLOAD'), params_list, facts)
+    print(params_list[0].location.name)
     optimal_plan = []
     return optimal_plan
 
